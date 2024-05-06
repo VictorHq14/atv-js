2d2cafb850d1cd2025573864ac17c984a2902a37
VictorHq14 <victorhugo.ce0212@gmail.com>
2024-05-06 18:51:30 -0300

    versâo 0.0.1
    atividade de classe da inf sch aula 1 de js

---
+++ 1-aula-js/index.js
@@ -0,0 +1,40 @@
+let adnaldo = [
+  'girafa',
+  'macaco',
+  189,
+  {
+    name: 'Marco',
+    age: 19,
+    isHuman: true
+  }, 
+  false
+]
+
+console.log(`ola ${adnaldo[1]} você tem ${adnaldo[3].age} anos`)
+
+// var name = prompt("Qual o seu nome?")
+// var age = prompt("Qual a sua idade?")
+
+// let info = {
+// name: name,
+// age: age,
+// }
+
+// console.log(`Olá ${info.name}, você tem ${info.age} anos `)
+
+// let name, age, isHuman
+
+// name = 'Pedro'
+// age = 12
+// isHuman = true
+
+// console.log(`ola, ${name} voce tem ${age} anos `)
+
+// let armando = {
+// name: 'Armando',
+// age: 18,
+// isHuman: true
+
+// }
+
+// console.log(`Olá, ${armando.name}, você tem ${armando.age} anos `)
